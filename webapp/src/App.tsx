40e7eb306bcd684a295953376bff47cf2dd4584d
Philipp Muens <philipp@muens.io>
2024-07-06 20:48:31 +0200

    Get circuit inputs via form submission

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,34 +1,42 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 import { CircuitInputs, getCircuitInputs } from "./utils";
-
-const SOD_BASE64_QUERY_PARAM_KEY = "sod-base64";
 
 function App() {
   const [inputs, setInputs] = useState<CircuitInputs>();
   const [sodData, setSodData] = useState<string | undefined>();
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const result = urlParams.get(SOD_BASE64_QUERY_PARAM_KEY);
-    if (result) {
-      setSodData(result);
-    }
-  }, []);
-
-  useEffect(() => {
     if (sodData) {
-      (async () => {
-        const inputs = await getCircuitInputs(sodData);
-        setInputs(inputs);
-      })();
+      (async () => {})();
     }
   }, [sodData]);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (!sodData) {
+      throw new Error("SOD Data is empty");
+    }
+
+    const inputs = await getCircuitInputs(sodData);
+    setInputs(inputs);
+  }
 
   return (
     <>
       <>{sodData && <p>{sodData}</p>}</>
       <>{inputs && <p>{JSON.stringify(inputs, null, 2)}</p>}</>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="SOD Data (in Base64)"
+          value={sodData}
+          onChange={(e) => setSodData(e.target.value)}
+        />
+        <button type="submit">Generate Circuit Inputs</button>
+      </form>
     </>
   );
 }
