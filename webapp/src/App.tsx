cff71d7f0093e9dacb4e87f474a6613afec78d58
Philipp Muens <philipp@muens.io>
2024-07-07 03:28:07 +0200

    Add snarkjs proving and verifying

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,8 +3,17 @@ import { FormEvent, useState } from "react";
 import "./App.css";
 import { getCircuitInputs } from "./utils";
 
+const WASM_URL = "/Euclid/circuit.wasm";
+const ZKEY_URL = "/Euclid/eu-verifier_0001.zkey";
+const VKEY_URL = "/Euclid/verification_key.json";
+
 function App() {
+  const [proof, setProof] = useState();
   const [sodData, setSodData] = useState<string>("");
+  const [inputJSON, setInputJSON] = useState<string>();
+  const [publicSignals, setPublicSignals] = useState();
+  const [isProving, setIsProving] = useState<boolean>(false);
+  const [isVerifying, setIsVerifying] = useState<boolean>(false);
   const [copiedToClipboard, setCopiedToClipboard] = useState<boolean>(false);
 
   async function handleSubmit(e: FormEvent) {
@@ -18,8 +27,62 @@ function App() {
     }
 
     const input = await getCircuitInputs(sodData);
-    navigator.clipboard.writeText(JSON.stringify(input));
+    const json = JSON.stringify(input);
+
+    setInputJSON(json);
+    navigator.clipboard.writeText(json);
     setCopiedToClipboard(true);
+  }
+
+  async function generateProof() {
+    if (!inputJSON) {
+      const error = new Error("Circuit inputs empty");
+      console.error(error);
+      alert(error.message);
+      return;
+    }
+
+    setIsProving(true);
+
+    const wasmFetch = await fetch(WASM_URL);
+    const wasmBuffer = await wasmFetch.arrayBuffer();
+    const wasm = new Uint8Array(wasmBuffer);
+
+    const zkeyFetch = await fetch(ZKEY_URL);
+    const zkeyBuffer = await zkeyFetch.arrayBuffer();
+    const zkey = new Uint8Array(zkeyBuffer);
+
+    // @ts-expect-error snarkjs is hosted as a static asset
+    const { proof, publicSignals } = await window.snarkjs.groth16.fullProve(
+      JSON.parse(inputJSON),
+      wasm,
+      zkey
+    );
+
+    setProof(proof);
+    setPublicSignals(publicSignals);
+
+    setIsProving(false);
+  }
+
+  async function verifyProof() {
+    if (!(proof || publicSignals)) {
+      const error = new Error("Proof not generated");
+      console.error(error);
+      alert(error.message);
+      return;
+    }
+
+    setIsVerifying(true);
+
+    const zkeyFetch = await fetch(VKEY_URL);
+    const vkey = await zkeyFetch.json();
+
+    // @ts-expect-error snarkjs is hosted as a static asset
+    const res = await window.snarkjs.groth16.verify(vkey, publicSignals, proof);
+
+    alert(`Proof Verification Result: ${res}`);
+    setIsVerifying(false);
   }
 
   return (
@@ -38,6 +101,23 @@ function App() {
         />
         <button type="submit">Get Circuit Inputs</button>
       </form>
+      <hr />
+      {proof && publicSignals && <p>Proof successfully generated</p>}
+      <button
+        type="button"
+        onClick={generateProof}
+        disabled={!inputJSON || isProving}
+      >
+        Generate Proof
+      </button>
+      <hr />
+      <button
+        type="button"
+        onClick={verifyProof}
+        disabled={!(proof && publicSignals) || isVerifying}
+      >
+        Verify Proof
+      </button>
     </div>
   );
 }
